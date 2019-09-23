358ebe1d18269910346667508009995ac17811b9
Juliano Krindges <julianokrindges@gmail.com>
2019-09-22 23:27:27 -0300

    feat: add header

--- src/index.js
+++ src/index.js
@@ -1,29 +1,10 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {View} from 'react-native';
 
 import './config/ReactotronConfig';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  welcome: {
-    fontSize: 20,
-    margin: 10,
-    alignItems: 'center',
-  },
-});
+import Routes from './routes';
 
-console.tron.log('Hello World');
-
-const App = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native</Text>
-    </View>
-  );
-};
-
-export default App;
+export default function App() {
+  return <Routes />;
+}
